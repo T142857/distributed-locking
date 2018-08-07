6984da350a395cd3a96ed818597d84856bd5a2df
toan-chev <toan.chev@gmail.com>
2018-08-07 13:58:28 +0700

    add file

---
+++ multiprocess/multithread.py
@@ -0,0 +1,29 @@
+import threading
+import time
+import random
+
+
+def hello(n):
+    time.sleep(random.randint(1, 3))
+    print("[{0}] Hello!".format(n))
+
+
+for i in range(10):
+    threading.Thread(target=hello, args=(i,)).start()
+
+print("Done!")
+
+"""
+the threads are running parallel, but not ORDER
+...>Done!
+[7] Hello!
+[8] Hello!
+[4] Hello!
+[3] Hello!
+[0] Hello!
+[2] Hello!
+[1] Hello!
+[9] Hello!
+[5] Hello!
+[6] Hello!
+"""
---
+++ multiprocess/multithread_order.py
@@ -0,0 +1,38 @@
+import threading
+import time
+import random
+
+
+def hello(n):
+    time.sleep(random.randint(1, 3))
+    print("[{0}] Hello!".format(n))
+
+
+threads = []
+for i in range(10):
+    t = threading.Thread(target=hello, args=(i,))
+    threads.append(t)
+    t.start()
+
+for one_thread in threads:
+    one_thread.join()
+
+print("Done!")
+
+"""
+if you want to be sure that "Done!"
+is printed after all the threads have finished running, you can use join
+...
+./multithread_order.py
+[2] Hello!
+[8] Hello!
+[7] Hello!
+[4] Hello!
+[5] Hello!
+[0] Hello!
+[6] Hello!
+[1] Hello!
+[9] Hello!
+[3] Hello!
+Done!
+"""
